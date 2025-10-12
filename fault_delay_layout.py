5372de50d3e8e3f173afac4fa6ebc3e6ef7d5c23
devmdave <davemadhav2007@gmail.com>
2025-10-12 11:37:34 +0530

    updated UI layout according to our theme still some fixes are left.

--- fault_delay_layout.py
+++ fault_delay_layout.py
@@ -21,6 +21,7 @@ custom_headers2 = ['Station', 'Fault Delay']
 class CurrentFaultDelay(QMainWindow):
     def __init__(self, backup_file=None):
         super().__init__()
+        self.dark_mode = True
         self.setWindowTitle("Production Analyser")
         self.setGeometry(100, 100, 900, 400)  # Decreased window size
 
@@ -32,19 +33,25 @@ class CurrentFaultDelay(QMainWindow):
         self.file_name_label.setFont(QFont('Arial', 12, QFont.Bold))
         self.file_name_label.setStyleSheet("padding: 8px; color: #1a237e; background: #e3f2fd; border-radius: 6px;")
 
+        # --- Mode toggle button ---
+        self.mode_toggle_btn = QPushButton("Switch to Light Mode")
+        self.mode_toggle_btn.clicked.connect(self._toggle_mode)
+
         # --- Back button ---
         self.back_button = QPushButton("Back")
         self.back_button.clicked.connect(self.close)
         self.back_button.setFixedWidth(100)
 
-        # --- Top layout for label and back button ---
+        # --- Top layout for label, toggle, and back button ---
         top_layout = QHBoxLayout()
         top_layout.addWidget(self.file_name_label, alignment=Qt.AlignLeft)
+        top_layout.addWidget(self.mode_toggle_btn, alignment=Qt.AlignCenter)
         top_layout.addWidget(self.back_button, alignment=Qt.AlignRight)
 
         # --- Main layout ---
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+        self.setStyleSheet(self._get_stylesheet())
         main_layout = QVBoxLayout(central_widget)
         main_layout.addLayout(top_layout)
 
@@ -59,14 +66,7 @@ class CurrentFaultDelay(QMainWindow):
         left_label = QLabel("Total Delay")
         left_label.setFont(QFont('Arial', 11, QFont.Bold))
         left_label.setAlignment(Qt.AlignCenter)
-        left_label.setStyleSheet("""
-            background-color: #0000FF;
-            color: white;
-            padding: 12px;
-            border-radius: 4px;
-            font-size: 14px;
-            font-weight: bold;
-        """)
+        left_label.setObjectName("frame_label")
         left_layout.addWidget(left_label)
         self.fault_delay_table = QTableWidget()
         self.fault_delay_table.setFixedHeight(300)
@@ -79,14 +79,7 @@ class CurrentFaultDelay(QMainWindow):
         right_label = QLabel("Total Station Fault")
         right_label.setFont(QFont('Arial', 11, QFont.Bold))
         right_label.setAlignment(Qt.AlignCenter)
-        right_label.setStyleSheet("""
-            background-color: #0000FF;
-            color: white;
-            padding: 12px;
-            border-radius: 4px;
-            font-size: 14px;
-            font-weight: bold;
-        """)
+        right_label.setObjectName("frame_label")
 
         right_layout.addWidget(right_label)
         self.station_fault_table = QTableWidget()
@@ -122,20 +115,6 @@ class CurrentFaultDelay(QMainWindow):
         #     self.load_data_to_view()
 
     def populate_table(self, table, df,custom_headers):
-        table.setStyleSheet("""
-            QTableWidget {
-                font-size: 14px;
-                background-color: #e0e0e0;
-                border: 1px solid #000000; 
-                gridline-color: 1px solid #000000;               
-                selection-background-color: #a0d7ff;
-            }
-            QHeaderView::section {
-                background-color: #e0e0e0;
-                font-weight: bold;
-                padding: 4px;
-            }
-                """)
         table.setRowCount(len(df))
         table.setColumnCount(len(df.columns))
         table.setHorizontalHeaderLabels(custom_headers)
@@ -145,6 +124,7 @@ class CurrentFaultDelay(QMainWindow):
                 item = QTableWidgetItem(str(df.iloc[row, col]))
                 item.setTextAlignment(Qt.AlignCenter)
                 table.setItem(row, col, item)
+        table.verticalHeader().setVisible(False)
         # table.resizeColumnsToContents()
     
     def fetch_data(self,dg):
@@ -199,3 +179,212 @@ class CurrentFaultDelay(QMainWindow):
         except FileNotFoundError as e:
             self.dg = Dialog()
             self.dg.show_error_dialog()
+
+    def _toggle_mode(self):
+        self.dark_mode = not self.dark_mode
+        if self.dark_mode:
+            self.mode_toggle_btn.setText("Switch to Light Mode")
+        else:
+            self.mode_toggle_btn.setText("Switch to Dark Mode")
+        self.setStyleSheet(self._get_stylesheet())
+
+    def _get_stylesheet(self):
+        if self.dark_mode:
+            return """
+                QWidget {
+                    background-color: #34495e;
+                    color: #ecf0f1;
+                    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+                }
+                QLabel {
+                    color: #ecf0f1;
+                }
+                QLabel#frame_label {
+                    background-color: #002A4D;
+                    color: white;
+                    padding: 12px;
+                    border-radius: 4px;
+                    font-size: 14px;
+                    font-weight: bold;
+                }
+                QPushButton {
+                    background-color: #002A4D;
+                    border: none;
+                    color: white;
+                    padding: 7px 14px;
+                    border-radius: 8px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #003A5D;
+                }
+                QTextEdit {
+                    background-color: #1e1e2f;
+                    color: #ecf0f1;
+                    font-family: Consolas, monospace;
+                    font-size: 11pt;
+                    padding: 8px;
+                }
+                QFrame {
+                    background-color: #2c3e50;
+                    border-radius: 10px;
+                    padding: 3px;
+                }
+                QMenuBar {
+                    color: #002A4D;
+                    background-color: #C6E5F5;  /* Dark blue-gray */
+                    font-weight: bold;
+                    color: #002A4D;
+                }
+                QMenuBar::item {
+                    spacing: 3px;
+                    padding: 4px 12px;
+                    color: #002A4D;
+                    background-color: #C6E5F5;  /* Slightly lighter */
+                }
+                QMenuBar::item:selected {
+                    color: #002A4D;
+                    background-color: #AAD8F0;  /* Hover color */
+                }
+                QMenuBar::item:pressed {
+                    color: #002A4D;
+                    background-color: #AAD8F0;  /* Clicked color */
+                }
+                QMenu {
+                    color: #002A4D;
+                    background-color: #C6E5F5;  /* Dropdown background */
+                }
+                QMenu::item {
+                    padding: 5px 20px;
+                    color: #002A4D;
+                    background-color: transparent;
+                }
+                QMenu::item:selected {
+                    background-color: #6ABBE5;
+                    color: black;
+                }
+                QTableWidget {
+                    font-size: 10px;
+                    background-color: #f5f5f5;
+                    color: #002A4D;
+                    gridline-color: #cccccc;
+                    selection-background-color: #d3d3d3;
+                    alternate-background-color: #f0f8ff;
+                    font-family: 'Segoe UI';
+                    border: 1px solid #cccccc;
+                    border-radius: 5px;
+                }
+                QTableWidget::item {
+                    padding: 5px;
+                }
+                QHeaderView::section {
+                    background-color: #002A4D;
+                    color: white;
+                    font-weight: bold;
+                    padding: 8px;
+                    border: none;
+                    border-bottom: 2px solid #aad8f0;
+                }
+                QHeaderView::section:hover {
+                    background-color: #003A5D;
+                }
+            """
+        else:
+            return """
+                QWidget {
+                    background-color: #FFFFFF;
+                    color: #002A4D;
+                    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+                }
+                QLabel {
+                    color: #002A4D;
+                }
+                QLabel#frame_label {
+                    background-color: #002A4D;
+                    color: white;
+                    padding: 12px;
+                    border-radius: 4px;
+                    font-size: 14px;
+                    font-weight: bold;
+                }
+                QPushButton {
+                    background-color: #002A4D;
+                    border: none;
+                    color: white;
+                    padding: 7px 14px;
+                    border-radius: 8px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #003A5D;
+                }
+                QTextEdit {
+                    background-color: #ffffff;
+                    color: #002A4D;
+                    font-family: Consolas, monospace;
+                    font-size: 11pt;
+                    padding: 8px;
+                }
+                QFrame {
+                    background-color: #C6E5F5;
+                    border-radius: 10px;
+                    padding: 5px;
+                }
+                QMenuBar {
+                    background-color: #C6E5F5;  /* Dark blue-gray */
+                    font-weight: bold;
+                    color: #002A4D;
+                }
+                QMenuBar::item {
+                    spacing: 3px;
+                    padding: 4px 12px;
+                    color: #002A4D;
+                    background-color: #C6E5F5;  /* Slightly lighter */
+                }
+                QMenuBar::item:selected {
+                    color: #002A4D;
+                    background-color: #AAD8F0;  /* Hover color */
+                }
+                QMenuBar::item:pressed {
+                    color: #002A4D;
+                    background-color: #AAD8F0;  /* Clicked color */
+                }
+                QMenu {
+                    background-color: #C6E5F5;  /* Dropdown background */
+                    color: #002A4D;
+                }
+                QMenu::item {
+                    padding: 5px 20px;
+                    color: #002A4D;
+                    background-color: transparent;
+                }
+                QMenu::item:selected {
+                    background-color: #6ABBE5;
+                    color: black;
+                }
+                QTableWidget {
+                    font-size: 10px;
+                    background-color: #f5f5f5;
+                    color: #002A4D;
+                    gridline-color: #cccccc;
+                    selection-background-color: #d3d3d3;
+                    alternate-background-color: #f0f8ff;
+                    font-family: 'Segoe UI';
+                    border: 1px solid #cccccc;
+                    border-radius: 5px;
+                }
+                QTableWidget::item {
+                    padding: 5px;
+                }
+                QHeaderView::section {
+                    background-color: #002A4D;
+                    color: white;
+                    font-weight: bold;
+                    padding: 8px;
+                    border: none;
+                    border-bottom: 2px solid #aad8f0;
+                }
+                QHeaderView::section:hover {
+                    background-color: #003A5D;
+                }
+            """
