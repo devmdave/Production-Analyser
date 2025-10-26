04a6ce45b219e276ab0bf49a935c7806bf10a649
devmdave <davemadhav2007@gmail.com>
2025-10-26 22:53:10 +0530

    solved pandas data frame error for Fault Delay..

--- my_plc.py
+++ my_plc.py
@@ -190,7 +190,10 @@ class data_writer:
         os.makedirs(directory, exist_ok=True)
         # Convert dictionary to DataFrame
         if len(tags_data) > 0:
-            df = pd.DataFrame(tags_data)
+            if directory != self.CYCLETIME_BACKUP_DIR:
+                df = pd.DataFrame([tags_data])
+            else:
+                df = pd.DataFrame(tags_data)
             df.index = range(1, len(df) + 1)
             # Transpose the DataFrame to have tags as rows and their values as columns
             df = df.transpose()
